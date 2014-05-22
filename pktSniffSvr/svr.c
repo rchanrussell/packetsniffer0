660d53e2326fff66bba712af7e5cb252a5e08063
Robert Chan Russell <robert.s.russell1@gmail.com>
2014-05-22 10:09:01 -0600

    Created own packet header structs, copies of BSD ones, to improve portability, at least with Linux. Moved structs and includes to a header file svr.h and created prototypes in svr.h. Moved main to top of svr.c for improved clarity

--- pktSniffSvr/svr.c
+++ pktSniffSvr/svr.c
@@ -2,61 +2,89 @@
  * svr.c - simple server for receiving packets and saving to file
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
+#include "svr.h"
 
-#include <sys/types.h>
-#include <sys/time.h>
-
-#include <pcap.h>
-#include <sys/socket.h>
-#include <signal.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <netinet/if_ether.h>
-
-/*
-#include <sys/ioctl.h>
-#include <net/bpf.h>
-#include <net/if.h>
-*/
-
-struct my_ip {
-  u_int8_t  ip_vhl; /* header lenght, version */
-#define IP_V(ip)  (((ip)->ip_vhl & 0xf0) >> 4)
-#define IP_HL(ip) ((ip)->ip_vhl & 0x0f)
-  u_int8_t  ip_tos; /* type of service */
-  u_int16_t ip_len; /* total length */
-  u_int16_t ip_id;  /* identification */
-  u_int16_t ip_off; /* fragment offset field */
-#define IP_DF 0x4000  /* don't fragment flag */
-#define IP_MF 0x2000  /* more fragments flag */
-#define IP_OFFMASK 0x1fff /* mask for fragmenting bits */
-  u_int8_t  ip_ttl; /* time to live */
-  u_int8_t  ip_p;   /* protocol */
-  u_int16_t ip_sum; /* checksum */
-  struct in_addr ip_src,ip_dst; /* source and destination addresses */
-};
-
-struct my_icmphdr { /* because netinet/ip_icmp.h has type issues */
-  u_char icmp_type;
-  u_char icmp_code;
-  u_short icmp_cksum;
-};
-
-/*
- * handle zombie processes after forked processes are complete
- */
-void sigchld_handler(int s)
+int main(int argc, char **argv)
 {
-  while(waitpid(-1, NULL, WNOHANG) > 0);
+  char *dev;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_t* descr;
+  struct bpf_program fp; /* hold compiled program */
+  bpf_u_int32 maskp; /* subnet mask */
+  bpf_u_int32 netp; /* ip */
+
+  if(argc != 4) {
+    fprintf(stdout, "Usage: %s numpackets device \"filter program\"\n", argv[0]);
+    fprintf(stdout, "32pkts on Ethernet for port 80 - Ex: %s 32 eth0 \"port 80\"\n",argv[0]);
+    fprintf(stdout, "2pkts on second port from machine ip - Ex: %s 2 en1 \"src 192.168.1.1\"\n",argv[0]);
+    fprintf(stdout, "64pkts on eth0 any port, in or out - Ex: %s 64 eth0 \"ip\"\n",argv[0]);
+    return 0;
+  }
+
+
+  /* grab device to peak into */
+  dev = pcap_lookupdev(errbuf); /* always grabs inactive ones,but code exits without */
+
+  if (dev == NULL)
+  {
+    printf("%s\n", errbuf);
+    exit(1);
+  }
+
+  dev = argv[2];
+  printf("DEV: %s\n", dev);
+
+  /* ask pcap for network addr and mask fo device */
+  pcap_lookupnet(dev,&netp,&maskp,errbuf);
+
+  /* open device for sniffing */
+  /*
+   * pcap_t *pcap_open_live(char *device,int snaplen, int prmisc,int to_ms,
+   *    char *ebuf)
+   *
+   *    snaplen - maximum size of packets to capture in bytes
+   *    promisc - set card in promiscuous mode?
+   *    to_ms   - time to wait for packets in miliseconds before read
+   *    times out
+   *    errbuf  - if something happens, place error string here
+   *
+   *    Note if you change "prmisc" param to anything other than zero, you will
+   *    get all packets your device sees, whether they are intendeed for you or
+   *    not!! Be sure you know the rules of the network you are running on
+   *    before you set your card in promiscuous mode!!    
+   */
+  /* set to permiscuous 1 if monitoring traffic to other machines, 0 for this machine */
+  descr = pcap_open_live(dev,BUFSIZ,1,100,errbuf);
+
+  if(descr == NULL)
+  {
+    printf("pcap_open_live(): %s\n", errbuf);
+    exit(1);
+  }
+
+  /* compile program, passed param, like host www.google.ca */
+  if (pcap_compile(descr,&fp,argv[3],0,netp) == -1) {
+    fprintf(stderr, "Error calling pcap_compile\n");
+    perror("pcap_compile");
+    exit(1);
+  }
+
+  /* set compiled program as filter */
+  if (pcap_setfilter(descr,&fp) == -1) {
+    fprintf(stderr, "Error setting filter\n");
+    perror("pcap_setfilter");
+    exit(1);
+  }
+
+  /* call pcap_loop() and pass callback function
+   * int pcap_loop(pcap_t *p, int cnt, pcap_handler callback, u_char *user)
+   */
+  pcap_loop(descr, atoi(argv[1]), my_callback, NULL);
+
+  fprintf(stdout, "\nDone processing packets\n");
+
+  pcap_close(descr);
+  return 0;
 }
 
 u_int16_t handle_ethernet (u_char *args, const struct pcap_pkthdr* pkthdr,
@@ -89,21 +117,21 @@ u_int16_t handle_ethernet (u_char *args, const struct pcap_pkthdr* pkthdr,
 /* tcp */
 u_char* handle_TCP (u_char *args, const struct pcap_pkthdr* pkthdr,
                    const u_char* packet) {
-  const struct tcphdr* tcp;
+  const struct my_tcphdr* tcp;
   u_int length = pkthdr->len;
   u_short srcPort;
   u_short dstPort;
-  tcp_seq seq;
-  tcp_seq ack;
+  u_int32_t seq;
+  u_int32_t ack;
   u_char flags;
   u_char hlen;
 
   /* move past ethernet and IP packet headers */
-  tcp = (struct tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct my_ip));
+  tcp = (struct my_tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct my_ip));
   length -= sizeof(struct ether_header) + sizeof(struct my_ip);
 
   /* ensure tcp is valid length */
-  if (length < sizeof(struct tcphdr)) {
+  if (length < sizeof(struct my_tcphdr)) {
     fprintf(stdout,"handle_TCP: truncated TCP packet %d\n", length);
     return NULL;
   }
@@ -132,25 +160,25 @@ u_char* handle_TCP (u_char *args, const struct pcap_pkthdr* pkthdr,
 /* udp */
 u_char* handle_UDP (u_char *args, const struct pcap_pkthdr* pkthdr,
                    const u_char* packet) {
-  const struct udphdr* udp;
+  const struct my_udphdr* udp;
   u_int length = pkthdr->len;
   u_short srcPort;
   u_short dstPort;
   u_short pktLen;
 
   /* move past ethernet and IP packet headers */
-  udp = (struct udphdr*)(packet + sizeof(struct ether_header) + sizeof(struct my_ip));
+  udp = (struct my_udphdr*)(packet + sizeof(struct ether_header) + sizeof(struct my_ip));
   length -= sizeof(struct ether_header) + sizeof(struct my_ip);
   pktLen = udp->uh_ulen;
 
   /* ensure udp is valid length */
-  if (length < sizeof(struct udphdr)) {
+  if (length < sizeof(struct my_udphdr)) {
     fprintf(stdout,"handle_UDP: truncated UDP packet %d\n", length);
     return NULL;
   }
 
   /* check header length */
-  if(pktLen < sizeof(struct udphdr)) {
+  if(pktLen < sizeof(struct my_udphdr)) {
     fprintf(stdout, "handle_UDP: bad-packetlen %d\n",pktLen);
   }
 
@@ -287,7 +315,6 @@ u_char* handle_IP (u_char *args, const struct pcap_pkthdr* pkthdr,
 }
 
 
-
 void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* packet)
 {
   u_int16_t type = handle_ethernet(args,pkthdr,packet);
@@ -303,87 +330,4 @@ void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* p
   fflush(stdout);
 }
 
-int main(int argc, char **argv)
-{
-  char *dev;
-  char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* descr;
-  struct bpf_program fp; /* hold compiled program */
-  bpf_u_int32 maskp; /* subnet mask */
-  bpf_u_int32 netp; /* ip */
-
-  if(argc != 4) {
-    fprintf(stdout, "Usage: %s numpackets device \"filter program\"\n", argv[0]);
-    fprintf(stdout, "32pkts on Ethernet for port 80 - Ex: %s 32 eth0 \"port 80\"\n",argv[0]);
-    fprintf(stdout, "2pkts on second port from machine ip - Ex: %s 2 en1 \"src 192.168.1.1\"\n",argv[0]);
-    fprintf(stdout, "64pkts on eth0 any port, in or out - Ex: %s 64 eth0 \"ip\"\n",argv[0]);
-    return 0;
-  }
-
-
-  /* grab device to peak into */
-  dev = pcap_lookupdev(errbuf); /* always grabs inactive ones,but code exits without */
-
-  if (dev == NULL)
-  {
-    printf("%s\n", errbuf);
-    exit(1);
-  }
-
-  dev = argv[2];
-  printf("DEV: %s\n", dev);
-
-  /* ask pcap for network addr and mask fo device */
-  pcap_lookupnet(dev,&netp,&maskp,errbuf);
-
-  /* open device for sniffing */
-  /*
-   * pcap_t *pcap_open_live(char *device,int snaplen, int prmisc,int to_ms,
-   *    char *ebuf)
-   *
-   *    snaplen - maximum size of packets to capture in bytes
-   *    promisc - set card in promiscuous mode?
-   *    to_ms   - time to wait for packets in miliseconds before read
-   *    times out
-   *    errbuf  - if something happens, place error string here
-   *
-   *    Note if you change "prmisc" param to anything other than zero, you will
-   *    get all packets your device sees, whether they are intendeed for you or
-   *    not!! Be sure you know the rules of the network you are running on
-   *    before you set your card in promiscuous mode!!    
-   */
-  /* set to permiscuous 1 if monitoring traffic to other machines, 0 for this machine */
-  descr = pcap_open_live(dev,BUFSIZ,1,100,errbuf);
-
-  if(descr == NULL)
-  {
-    printf("pcap_open_live(): %s\n", errbuf);
-    exit(1);
-  }
-
-  /* compile program, passed param, like host www.google.ca */
-  if (pcap_compile(descr,&fp,argv[3],0,netp) == -1) {
-    fprintf(stderr, "Error calling pcap_compile\n");
-    perror("pcap_compile");
-    exit(1);
-  }
-
-  /* set compiled program as filter */
-  if (pcap_setfilter(descr,&fp) == -1) {
-    fprintf(stderr, "Error setting filter\n");
-    perror("pcap_setfilter");
-    exit(1);
-  }
-
-  /* call pcap_loop() and pass callback function
-   * int pcap_loop(pcap_t *p, int cnt, pcap_handler callback, u_char *user)
-   */
-  pcap_loop(descr, atoi(argv[1]), my_callback, NULL);
-
-  fprintf(stdout, "\nDone processing packets\n");
-
-  pcap_close(descr);
-  return 0;
-
-}
 
---
+++ pktSniffSvr/svr.h
@@ -0,0 +1,129 @@
+/*
+ * svr.h - simple server header file for receiving packets and saving to file
+ */
+#ifndef _PKTSNIFFSVR_H_
+#define _PKTSNIFFSVR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <pcap.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+#ifdef _NETINET_ETHER_H
+#include <netinet/ether.h>
+#else
+#include <netinet/if_ether.h>
+#endif
+
+/*
+ * Structures
+ * write own to improve portability
+ * mostly are direct copies of BSD
+ */
+
+struct my_ip {
+  u_int8_t  ip_vhl; /* header lenght, version */
+#define IP_V(ip)  (((ip)->ip_vhl & 0xf0) >> 4)
+#define IP_HL(ip) ((ip)->ip_vhl & 0x0f)
+  u_int8_t  ip_tos; /* type of service */
+  u_int16_t ip_len; /* total length */
+  u_int16_t ip_id;  /* identification */
+  u_int16_t ip_off; /* fragment offset field */
+#define IP_DF 0x4000  /* don't fragment flag */
+#define IP_MF 0x2000  /* more fragments flag */
+#define IP_OFFMASK 0x1fff /* mask for fragmenting bits */
+  u_int8_t  ip_ttl; /* time to live */
+  u_int8_t  ip_p;   /* protocol */
+  u_int16_t ip_sum; /* checksum */
+  struct in_addr ip_src,ip_dst; /* source and destination addresses */
+};
+
+struct my_icmphdr { /* because netinet/ip_icmp.h has type issues */
+  u_char icmp_type;
+  u_char icmp_code;
+  u_short icmp_cksum;
+};
+
+struct my_tcphdr { /* try to improve portability since this won't work on Linux */
+  u_short th_sport;
+  u_short th_dport;
+  u_int32_t th_seq;
+  u_int32_t th_ack;
+#if BYTE_ORDER == LITTLE_ENDIAN
+  u_char th_x2:4,
+         th_off:4;
+#endif
+#if BYTE_ORDER == BIG_ENDIAN
+  u_char th_off:4,
+         th_x2:4;
+#endif
+  u_char th_flags;
+  u_short th_win;
+  u_short th_sum;
+  u_short th_urp;
+};
+
+struct my_udphdr {
+  u_short uh_sport;
+  u_short uh_dport;
+  u_short uh_ulen;
+  u_short uh_sum;
+};
+
+/*
+ * Function Prototypes 
+ */
+/*
+ * Ethernet packet
+ */
+u_int16_t handle_ethernet (u_char *args, const struct pcap_pkthdr* pkthdr,
+                           const u_char* packet); 
+  
+/* 
+ * TCP packet
+ */
+u_char* handle_TCP (u_char *args, const struct pcap_pkthdr* pkthdr,
+                   const u_char* packet);
+
+/*
+ * UDP packet
+ */
+u_char* handle_UDP (u_char *args, const struct pcap_pkthdr* pkthdr,
+                   const u_char* packet);
+
+/*
+ * ICMP packet
+ */
+u_char* handle_ICMP (u_char *args, const struct pcap_pkthdr* pkthdr,
+                   const u_char* packet);
+
+/*
+ * IP packet
+ */
+u_char* handle_IP (u_char *args, const struct pcap_pkthdr* pkthdr,
+                   const u_char* packet);
+
+/*
+ * Process Protocl Selector
+ */
+u_char* getProtocol (u_int8_t protocol, u_char *args, const struct pcap_pkthdr* pkthdr,
+                     const u_char* packet);
+
+/*
+ * Callback used by pcap_loop
+ */
+void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* packet);
+
+#endif
